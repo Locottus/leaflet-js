11e0f1b1a966663d7038ab0e17b460931e00aa0d
Locottus <herlich@gmail.com>
2021-10-01 07:30:59 -0600

    final map display fixes

--- index.js
+++ index.js
@@ -1,99 +1,120 @@
-var OWM_API_KEY = '98674de6a91859bcea48ba07be964379';
-console.log("hola mundo!");
-
+var OWM_API_KEY = "98674de6a91859bcea48ba07be964379";
+console.log("start of line");
 
 //main map layer
-var map = L.map('map').setView([14.628434,-90.522713], 7);
+var map = L.map("map").setView([14.628434, -90.522713], 7);
 
 //osm layer ****MAIN LAYER***
-var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+var osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 });
 osm.addTo(map);
 
-
-// google street 
-googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-maxZoom: 20,
-subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-});
+// google street
+googleStreets = L.tileLayer(
+  "http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}",
+  {
+    maxZoom: 20,
+    subdomains: ["mt0", "mt1", "mt2", "mt3"],
+  }
+);
 //googleStreets.addTo(map);
 
-var humanitarian = L.tileLayer('https://tile-{s}.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+var humanitarian = L.tileLayer(
+  "https://tile-{s}.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+  {
     maxZoom: 17,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors</a> <a href="https://www.hotosm.org/" target="_blank">Tiles courtesy of Humanitarian OpenStreetMap Team</a>'
-    });
-    //humanitarian.addTo(map);
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors</a> <a href="https://www.hotosm.org/" target="_blank">Tiles courtesy of Humanitarian OpenStreetMap Team</a>',
+  }
+);
+//humanitarian.addTo(map);
 
-//*********************WEATHER TILES*************************** */
-var clouds = L.OWM.clouds({opacity: 0.8, legendImagePath: 'files/NT2.png', appId: OWM_API_KEY});
-var cloudscls = L.OWM.cloudsClassic({opacity: 0.5, appId: OWM_API_KEY});
-var precipitation = L.OWM.precipitation( {opacity: 0.5, appId: OWM_API_KEY} );
-var precipitationcls = L.OWM.precipitationClassic({opacity: 0.5, appId: OWM_API_KEY});
-var rain = L.OWM.rain({opacity: 0.5, appId: OWM_API_KEY});
-var raincls = L.OWM.rainClassic({opacity: 0.5, appId: OWM_API_KEY});
-var snow = L.OWM.snow({opacity: 0.5, appId: OWM_API_KEY});
-var pressure = L.OWM.pressure({opacity: 0.4, appId: OWM_API_KEY});
-var pressurecntr = L.OWM.pressureContour({opacity: 0.5, appId: OWM_API_KEY});
-var temp = L.OWM.temperature({opacity: 0.5, appId: OWM_API_KEY});
-var wind = L.OWM.wind({opacity: 0.5, appId: OWM_API_KEY});
+var dark = L.tileLayer(
+  "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+  {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: "abcd",
+    maxZoom: 19,
+  }
+);
 
-//set clickable event -->left click
-map.on('click', function(e)  { 
-    console.log("clicked map ",e.latlng/*.lat,e.latlng.lng*/); 
-
+googleSat = L.tileLayer("http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}", {
+  maxZoom: 20,
+  subdomains: ["mt0", "mt1", "mt2", "mt3"],
 });
 
-//set clickable event --> right click
-map.on('contextmenu', function(e) {
-    //alert(e.latlng);
-    
-    window.showAlert = function(){
-        var forma = `
-        
+//*********************WEATHER TILES*************************** */
+var clouds = L.OWM.clouds({
+  opacity: 0.8,
+  legendImagePath: "files/NT2.png",
+  appId: OWM_API_KEY,
+});
+var cloudscls = L.OWM.cloudsClassic({ opacity: 0.5, appId: OWM_API_KEY });
+var precipitation = L.OWM.precipitation({ opacity: 0.5, appId: OWM_API_KEY });
+var precipitationcls = L.OWM.precipitationClassic({
+  opacity: 0.5,
+  appId: OWM_API_KEY,
+});
+var rain = L.OWM.rain({ opacity: 0.5, appId: OWM_API_KEY });
+var raincls = L.OWM.rainClassic({ opacity: 0.5, appId: OWM_API_KEY });
+var snow = L.OWM.snow({ opacity: 0.5, appId: OWM_API_KEY });
+var pressure = L.OWM.pressure({ opacity: 0.4, appId: OWM_API_KEY });
+var pressurecntr = L.OWM.pressureContour({ opacity: 0.5, appId: OWM_API_KEY });
+var temp = L.OWM.temperature({ opacity: 0.5, appId: OWM_API_KEY });
+var wind = L.OWM.wind({ opacity: 0.5, appId: OWM_API_KEY });
+
+/*==============================================
+                    LAYER CONTROL
+    ================================================*/
+var baseMaps = {
+  OSM: osm,
+  "Google Street Map": googleStreets,
+  Humanitarian: humanitarian,
+  "Mapa Obscuro": dark,
+  "Google Satellite": googleSat,
+
+  //"CLOUDS": clouds
+};
+
+var layers = {
+  "Nubes clasico": cloudscls,
+  "Nuebes": clouds,
+  "Precipitacion": precipitation,
+  "Precipitacion clasico": precipitationcls,
+  "Lluvia": rain,
+  "Lluvia clasico": raincls,
+  "Nieve": snow,
+  "Presion": pressure,
+  "Presion cntr": pressurecntr,
+  "Temperatura": temp,
+  "Viento": wind,
+};
+
+L.control.layers(baseMaps, layers).addTo(map);
+
+//set clickable event -->left click
+map.on("click", function (e) {
+  console.log("clicked map ", e.latlng /*.lat,e.latlng.lng*/);
+});
+
+map.on("contextmenu", function (e) {
+
+  window.showAlert = function () {
+    var forma = `
+        <i class="fas fa-anchor"></i>
         <button type="button" class="btn btn-primary" onclick = "alert('clicked')">Primary</button>
         <button type="button" class="btn btn-secondary">Secondary</button>
         <button type="button" class="btn btn-success">Success</button>
         <button type="button" class="btn btn-danger">Danger</button>
         
         `;
-        alertify.alert(forma);
-    }
+    alertify.alert(forma);
+  };
 
-     window.showAlert();
-
+  window.showAlert();
 });
-
-
-//var layerControl = L.control.layers(baseMaps, overlayMaps, {collapsed: true}).addTo(map);
-
-
-    /*==============================================
-                    LAYER CONTROL
-    ================================================*/
-    var baseMaps = {
-        "OSM": osm,
-        "Google Street": googleStreets,
-        "Humanitarian" : humanitarian,
-        
-        //"CLOUDS": clouds
-    };
-
-    var layers = {
-        "Nubes cls":cloudscls,
-        "Nuebes" :clouds,
-        "precipitacion": precipitation,
-        "Precipitacion cls" :precipitationcls,
-        "lluvia":rain,
-        "lluvia cls":raincls,
-        "nieve":snow,
-        "presion":pressure,
-        "presion cntr":pressurecntr,
-        "temperatura":temp,
-        "viento":wind
-    }
-
-    L.control.layers(baseMaps,layers).addTo(map);
-    //map.addLayer(clouds);
 
 console.log("end of line");
